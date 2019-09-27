c23bd4e71821358606c7059694ce1b99b8fd917b
Sada <samasunari@gmail.com>
2019-09-28 05:27:30 +0900

    episode31 done

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { signIN } from '../../store/actions/authActions'
+
+import {signUp} from '../../store/actions/authActions'
+import authReducer from '../../store/reducers/authReducer';
 
 class SignUp extends Component {
     state = {
@@ -18,15 +20,16 @@ class SignUp extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.signUp(this.state)
+        
     }
     render() {
-        const { auth } = this.props;
+        const { auth, authError } = this.props;
         if (auth.id) return <Redirect to='/' />
         return (
             <div className="container">
              <form onSubmit={this.handleSubmit} className="white">
-              <h5 className="grey-text text-darken-3">Sign IN</h5>
+              <h5 className="grey-text text-darken-3">Sign UP</h5>
               <div className="input-field">
                <label htmlFor="email">Email</label>
                <input type="email" id="email" onChange={this.handleChange} />
@@ -45,6 +48,9 @@ class SignUp extends Component {
                </div>
               <div className="input-field">
                <button className="btn pink lighten-1 z-depth-0">SignUp</button>
+               <div className="red-text center">
+                { authError? <p>{ authError }</p> : null}
+               </div>
               </div>
              </form>
             </div>
@@ -53,8 +59,15 @@ class SignUp extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
     }
 }
 
-export default connect(mapStateToProps)(SignUp)
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import moment from 'moment'
+
 
 const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
          <div className="card-content grey-text text-darken-3">
           <span className="card-title">{project.title}</span>
-          <p className="grey-text">3rd September, 2am</p>
+          <p>Posted by {project.authorFirstName}</p>
+          <p className="grey-text"></p>
          </div>
         </div>
     )
