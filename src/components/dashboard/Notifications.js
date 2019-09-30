e11fde8ebe6ca7ecf21e8119ce148ab104e9ede3
Sada <samasunari@gmail.com>
2019-09-30 23:56:45 +0900

    episode 37 done

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Notifications = () => {
+const Notifications = (props) => {
     return(
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+         <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+          <div class="card-content white-text">
+           <span className="card-title">Notifications</span>
+             <ul className="notifications">
+              <li>NOtification</li>
+              <li>NOtification</li>
+              <li>NOtification</li>
+             <li>NOtification</li>
+            </ul>
+           </div>
+          </div>
+         </div>
         </div>
     )
 }
